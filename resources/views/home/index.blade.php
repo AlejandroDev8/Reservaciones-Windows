91ec3bdca625dd3fd7c25996193b4b4f295ecfca
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2024-05-21 16:28:33 -0600

    feat: Add home controller and view for homepage

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,13 @@
+<x-app-layout>
+  <div class=" max-w-xl mx-auto px-4 sm:px-6 lg:px-8 lg:max-w-7xl">
+    <div class="relative mt-10">
+      <h2 class="text-center text-4xl leading-8 font-extrabold tracking-tight text-indigo-600 sm:text-6xl">Haz tu
+        reservaciones para el uso de las salas del TecNM-Ciudad Valles</h2>
+      <p class="mt-4 max-w-3xl mx-auto text-center text-xl text-gray-500">
+        El Tecnológico Nacional de México en Ciudad Valles, pone a disposición de la comunidad estudiantil y
+        docente
+        las salas de estudio, para que puedan realizar sus actividades académicas.
+      </p>
+    </div>
+  </div>
+</x-app-layout>
