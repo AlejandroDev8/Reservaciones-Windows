739458604d5914ebb6061ba1ad136984daa0a0f2
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2024-05-21 16:32:46 -0600

    chore: Update login page with Spanish translations and UI improvements

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,49 @@
 <x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+  <!-- Session Status -->
+  <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+  <form method="POST" action="{{ route('login') }}" novalidate>
+    @csrf
+    <x-input-error :messages="str_replace('email', 'correo electrónico', $errors->get('email'))" class="mt-2" />
+    <x-input-error :messages="str_replace('password', 'contraseña', $errors->get('password'))" class="mt-2" />
+    <!-- Email Address -->
+    <div>
+      <x-input-label for="email" :value="__('Correo Eléctronico')" />
+      <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required
+        autofocus autocomplete="username" />
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+    </div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+    <!-- Password -->
+    <div class="mt-4">
+      <x-input-label for="password" :value="__('Contraseña')" />
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+      <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
+        autocomplete="current-password" />
+    </div>
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+    <!-- Remember Me -->
+    <div class="block mt-4">
+      <label for="remember_me" class="inline-flex items-center">
+        <input id="remember_me" type="checkbox"
+          class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
+        <span class="ms-2 text-sm text-gray-600">{{ __('Recordarme') }}</span>
+      </label>
+    </div>
 
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
+    <div class="flex justify-between my-5">
 
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
+      <x-link href="{{ route('register') }}">
+        Crear Cuenta
+      </x-link>
 
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
+      <x-link href="{{ route('password.request') }}">
+        Olvidaste tu contraseña
+      </x-link>
+
+    </div>
+    <x-primary-button class="w-full justify-center">
+      {{ __('Inicar Sesión') }}
+    </x-primary-button>
+  </form>
 </x-guest-layout>
