5e31048a248f9698dfb0d88cff42cbfcbbad5f5d
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2024-05-21 19:10:36 -0600

    feat: Add HelperController with index and users methods

---
+++ app/Http/Controllers/HelperController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HelperController extends Controller
+{
+    public function index()
+    {
+        return view('help.index');
+    }
+
+    public function users()
+    {
+        return view('help.users');
+    }
+}
