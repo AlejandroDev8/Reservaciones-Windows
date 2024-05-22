2749ff9c94c61dbf245e9b8b35cdc20325c70278
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2024-05-21 19:23:31 -0600

    feat: Update MostrarSolicitudesAdmin component to include search functionality

--- app/Livewire/MostrarSolicitudesAdmin.php
+++ app/Livewire/MostrarSolicitudesAdmin.php
@@ -2,12 +2,33 @@
 
 namespace App\Livewire;
 
+use App\Models\Reservation;
 use Livewire\Component;
 
 class MostrarSolicitudesAdmin extends Component
 {
+    public $sala;
+    public $user;
+    public $estado;
+
+    protected $listeners = ['terminosBusqueda' => 'buscar'];
+
+    public function buscar($sala, $user, $estado)
+    {
+        $this->sala = $sala;
+        $this->user = $user;
+        $this->estado = $estado;
+    }
+
     public function render()
     {
-        return view('livewire.mostrar-solicitudes-admin');
+        $solicitudes = Reservation::where('sala_id', 'like', '%' . $this->sala . '%')
+            ->where('user_id', 'like', '%' . $this->user . '%')
+            ->where('estado_id', 'like', '%' . $this->estado . '%')
+            ->get();
+
+        return view('livewire.mostrar-solicitudes-admin', [
+            'solicitudes' => $solicitudes
+        ]);
     }
 }
--- resources/views/livewire/mostrar-solicitudes-admin.blade.php
+++ resources/views/livewire/mostrar-solicitudes-admin.blade.php
@@ -1,3 +1,55 @@
 <div>
-    {{-- Stop trying to control. --}}
+    <livewire:filtrar-solicitudes />
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto">
+            <h3 class="font-extrabold text-4xl text-gray-700 mb-12">Solicitudes</h3>
+            <div class="bg-white shadow-sm rounded-lg p-6 divide-y divide-gray-200">
+                @forelse ($solicitudes as $solicitud)
+                <div class="md:flex md:justify-between md:items-center py-5">
+                    <div class="md:flex-1">
+                        <p class="text-3xl font-extrabold text-gray-600 mb-1">Sala Solicitada:
+                            <span class="text-indigo-600 normal-case">
+                                {{ $solicitud->sala->salas}}
+                            </span>
+                        </p>
+                        <p class="text-base text-gray-600 mb-1">Nombre del solicitante:
+                            <span class="text-indigo-600 normal-case">{{$solicitud->user->name}}</span>
+                        </p>
+                        <p class="text-base text-gray-600 mb-1">Correo electrónico asociado: <span
+                                class="text-indigo-600 normal-case">{{ $solicitud->email }}</span></p>
+                        <p class="text-base text-gray-600 mb-1">Fecha de Inicio de la Reservación:
+                            <span class="text-indigo-600 normal-case">
+                                {{ \Carbon\Carbon::parse($solicitud->fecha_inicio)->format('d/m/Y') }}
+                            </span>
+                        </p>
+                        <p class="text-base text-gray-600 mb-1">Fecha de Fin de la Reservación:
+                            <span class="text-indigo-600 normal-case">
+                                {{ \Carbon\Carbon::parse($solicitud->fecha_fin)->format('d/m/Y') }}
+                            </span>
+                        </p>
+                        <p class="text-base text-gray-600 mb-1">Estado:
+                            <span class="text-indigo-600 normal-case">
+                                {{$solicitud->estados->estados}}
+                            </span>
+                        </p>
+                    </div>
+                    @if ($solicitud->estado_id === 1)
+                    <div class="mt-5 md:mt-0">
+                        <a href="{{route('reservaciones.aceptar', $solicitud->id)}}"
+                            class="items-center px-4 py-3 bg-indigo-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-indigo-700 focus:bg-indigo-700 active:bg-indigo-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150 block text-center">Ver
+                            Solicitud (Aceptar Solicitud)
+                        </a>
+                        <a href="{{route('reservaciones.rechazar', $solicitud->id)}}"
+                            class="mt-3 items-center px-4 py-3 bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-700 focus:bg-red-700 active:bg-red-900 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition ease-in-out duration-150 block text-center">Ver
+                            Solicitud (Rechazar Solicitud)
+                        </a>
+                    </div>
+                    @endif
+                </div>
+                @empty
+                <p class="p-3 text-center text-2xl text-gray-600">No hay Solicitudes aún</p>
+                @endforelse
+            </div>
+        </div>
+    </div>
 </div>
