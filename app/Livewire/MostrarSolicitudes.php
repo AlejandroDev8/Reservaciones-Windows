0aa85d499f634440ecaaeaa190787b4bd5b2e1f0
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2024-05-21 19:21:05 -0600

    refactor: Update MostrarSolicitudes component to include delete functionality

--- app/Livewire/MostrarSolicitudes.php
+++ app/Livewire/MostrarSolicitudes.php
@@ -2,12 +2,25 @@
 
 namespace App\Livewire;
 
+use App\Models\Reservation;
 use Livewire\Component;
 
 class MostrarSolicitudes extends Component
 {
+
+    protected $listeners = ['eliminarSoliitud'];
+
+    public function eliminarSolicitud(Reservation $reservacion)
+    {
+        $reservacion->delete();
+    }
+
     public function render()
     {
-        return view('livewire.mostrar-solicitudes');
+        $solicitudes = Reservation::where('user_id', auth()->user()->id)->paginate(10);
+
+        return view('livewire.mostrar-solicitudes', [
+            'solicitudes' => $solicitudes
+        ]);
     }
 }
