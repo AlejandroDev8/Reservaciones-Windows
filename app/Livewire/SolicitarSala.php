7bb2180524c47d4540139081f218efb2f735ac01
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2024-05-22 00:46:28 -0600

    feat: Add form and functionality for requesting a room reservation

--- app/Livewire/SolicitarSala.php
+++ app/Livewire/SolicitarSala.php
@@ -2,12 +2,87 @@
 
 namespace App\Livewire;
 
+use App\Models\Sala;
+use App\Models\Acomodo;
 use Livewire\Component;
+use App\Models\Reservation;
 
 class SolicitarSala extends Component
 {
+    public $email;
+    public $sala;
+    public $fecha_inicio;
+    public $fecha_fin;
+    public $acomodo;
+    public $extras;
+
+    public function mount()
+    {
+        $this->email = auth()->user()->email;
+        $this->sala = '';
+        $this->acomodo = '';
+    }
+
+    public function solicitarSala()
+    {
+        $this->validate([
+            'email' => 'required|email',
+            'sala' => 'required|numeric|between:1,3',
+            'fecha_inicio' => 'required|date',
+            'fecha_fin' => 'required|date|after_or_equal:fecha_inicio',
+            'acomodo' => 'required|numeric|between:1,3',
+            'extras' => 'max:100',
+        ]);
+
+        // Verificar si las fechas están disponibles para la sala seleccionada
+        $reservas = Reservation::where('sala_id', $this->sala)
+            ->where(function ($query) {
+                $query->whereBetween('fecha_inicio', [$this->fecha_inicio, $this->fecha_fin])
+                    ->orWhereBetween('fecha_fin', [$this->fecha_inicio, $this->fecha_fin])
+                    ->orWhere(function ($query) {
+                        $query->where('fecha_inicio', '<=', $this->fecha_inicio)
+                            ->where('fecha_fin', '>=', $this->fecha_fin);
+                    });
+            })
+            ->exists();
+
+        if ($reservas) {
+            $this->addError('fecha_inicio', 'Las fechas seleccionadas ya han sido reservadas para esta sala.');
+            $this->addError('fecha_fin', 'Las fechas seleccionadas ya han sido reservadas para esta sala.');
+            return;
+        }
+
+        // Crear la reserva
+        Reservation::create([
+            'email' => $this->email,
+            'sala_id' => $this->sala,
+            'fecha_inicio' => $this->fecha_inicio,
+            'fecha_fin' => $this->fecha_fin,
+            'acomodo_id' => $this->acomodo,
+            'extras' => $this->extras,
+            'user_id' => auth()->user()->id,
+        ]);
+
+        session()->flash('message', 'La solicitud se ha registrado correctamente');
+        return redirect()->route('reservations.index');
+    }
+
     public function render()
     {
-        return view('livewire.solicitar-sala');
+
+        $minDate = date('Y-m-d');
+        $maxDate = date('Y-m-d', strtotime('+1 year'));
+
+        $user = auth()->user();
+        $salas = Sala::all();
+        $acomodos = Acomodo::all();
+
+        return view('livewire.solicitar-sala', [
+            'salas' => $salas,
+            'acomodos' => $acomodos,
+            'userEmail' => $user->email,
+            'minDate' => $minDate,
+            'maxDate' => $maxDate,
+        ]);
     }
 }
--- resources/views/livewire/solicitar-sala.blade.php
+++ resources/views/livewire/solicitar-sala.blade.php
@@ -1,3 +1,67 @@
-<div>
-    {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
-</div>
+<form class="md:w-1/2 space-y-5" wire:submit.prevent='solicitarSala'>
+    <h1 class="font-bold text-lg mb-3">Formulario de Reservación</h1>
+    {{--
+    <x-input-error :messages="str_replace('email', 'correo electrónico', $errors->get('email'))" class="mt-2" /> --}}
+    <div>
+        <x-input-label for="email" :value="__('Correo Electrónico')" />
+        <x-text-input id="email" class="block mt-1 w-full" type="email" wire:model="email" :value="$userEmail"
+            placeholder="Correo electrónico para mandar la confirmación de la reservación" disabled />
+        @error('email')
+        <livewire:mostrar-alerta :message="str_replace('email', 'correo electrónico', $message)" />
+        @enderror
+    </div>
+    <div>
+        <x-input-label for="sala" :value="__('Seleccione una sala')" />
+        <select wire:model="sala" id="sala"
+            class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-300 focus:ring-opacity-50 w-full">
+            <option value="" disabled selected>-- Seleccione una sala --</option>
+            @foreach ($salas as $sala)
+            <option value="{{$sala->id}}">{{$sala->salas}}</option>
+            @endforeach
+        </select>
+        @error('sala')
+        <livewire:mostrar-alerta :message="$message" />
+        @enderror
+    </div>
+    <div>
+        <x-input-label for="fecha_inicio" :value="__('Seleccione una Fecha de inicio')" />
+        <x-text-input id="fecha_inicio" class="block mt-1 w-full" type="date" wire:model="fecha_inicio"
+            :value="old('fecha_inicio')" min="{{$minDate}}" max="{{$maxDate}}" />
+        @error('fecha_inicio')
+        <livewire:mostrar-alerta :message="$message" />
+        @enderror
+    </div>
+    <div>
+        <x-input-label for="fecha_fin" :value="__('Seleccione una Fecha de fin')" />
+        <x-text-input id="fecha_fin" class="block mt-1 w-full" type="date" wire:model="fecha_fin"
+            :value="old('fecha_fin')" min="{{$minDate}}" max="{{$maxDate}}" />
+        @error('fecha_fin')
+        <livewire:mostrar-alerta :message="$message" />
+        @enderror
+    </div>
+    <div>
+        <x-input-label for="sillas" :value="__('Seleccione el acomodo de sillas')" />
+        <select wire:model="acomodo" id="sillas"
+            class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-300 focus:ring-opacity-50 w-full">
+            <option value="" disabled selected>-- Seleccione un acomodo --</option>
+            @foreach ($acomodos as $acomodo)
+            <option value="{{$acomodo->id}}">{{$acomodo->acomodo}}</option>
+            @endforeach
+        </select>
+        @error('acomodo')
+        <livewire:mostrar-alerta :message="str_replace('acomodo', 'acomo de de sillas', $message)" />
+        @enderror
+    </div>
+    <div>
+        <x-input-label for="extras" :value="__('Especificaciones extras')" />
+        <textarea wire:model="extras" id="extras" cols="30" rows="10"
+            class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-300 focus:ring-opacity-50 w-full"
+            placeholder="Especificacione extras para añadir a la sala (Limpieza, más sillas y/o mesas, etc..)"></textarea>
+        @error('extras')
+        <livewire:mostrar-alerta :message="$message" />
+        @enderror
+    </div>
+    <x-primary-button>
+        {{ __('Enviar Solicitud') }}
+    </x-primary-button>
+</form>
