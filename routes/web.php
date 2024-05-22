4a6bdb3b6dcb856fcad843f13f88dc91cb867746
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2024-05-21 19:11:48 -0600

    feat: Add routes for help page and reservations creation

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HelperController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ReservationController;
 use Illuminate\Support\Facades\Route;
@@ -8,11 +9,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/help', [HelperController::class, 'index'])->name('help.index');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('/dashboard', [ReservationController::class, 'index'])->name('dashboard');
+    Route::get('/dashboard', [ReservationController::class, 'index'])->name('reservations.index');
+    Route::get('/reservations/create', [ReservationController::class, 'create'])->name('reservations.create');
+    Route::get('/help/users', [HelperController::class, 'users'])->name('help.users');
 });
 
 require __DIR__ . '/auth.php';
