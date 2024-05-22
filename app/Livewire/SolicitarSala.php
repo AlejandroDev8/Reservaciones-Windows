e9c9dc082c29849e47144d31a9259fd1f572f90e
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2024-05-21 20:07:47 -0600

    feat: Add Livewire component for requesting a room

---
+++ app/Livewire/SolicitarSala.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class SolicitarSala extends Component
+{
+    public function render()
+    {
+        return view('livewire.solicitar-sala');
+    }
+}
