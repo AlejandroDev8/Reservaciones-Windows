24b5824583217317f8d6c8b66b05981ba6b91acc
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2024-06-04 18:40:13 -0600

    refactor: Update homepage route to use HomeController

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,12 @@
 <?php
 
 use App\Http\Controllers\HelperController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ReservationController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', HomeController::class)->name('home');
 
 Route::get('/help', [HelperController::class, 'index'])->name('help.index');
 
