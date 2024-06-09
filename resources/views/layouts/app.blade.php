17f25e66da5831b992709c3adefd492f65e2626d
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2024-06-08 18:29:38 -0600

    refactor: Update favicon link to use .ico file format

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <link rel="icon" href="{{ asset('img/icon.webp') }}" type="image/webp">
+    <link rel="shortcut icon" href="{{ asset('img/icon.ico') }}" type="image/x-icon">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
