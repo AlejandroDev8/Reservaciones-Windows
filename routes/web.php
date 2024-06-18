3e69e89c8102258b1cfaea4f693cf0367a33623f
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2024-06-17 19:35:00 -0600

    refactor: Add authenticated dashboard route and update profile routes

--- routes/web.php
+++ routes/web.php
@@ -10,14 +10,11 @@ Route::get('/', HomeController::class)->name('home');
 
 Route::get('/help', [HelperController::class, 'index'])->name('help.index');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('reservations.index');
-
-Route::middleware('auth')->group(function () {
+Route::middleware('auth', 'verified')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/dashboard', [ReservationController::class, 'index'])->name('reservations.index');
     Route::get('/reservations/create', [ReservationController::class, 'create'])->name('reservations.create');
     Route::get('/reservaciones/{reservacion}/aceptar-solicitud', [ReservationController::class, 'aceptarSolicitud'])->name('reservaciones.aceptar-solicitud');
     Route::get('/reservaciones/{reservacion}/rechazar-solicitud', [ReservationController::class, 'rechazarSolicitud'])->name('reservaciones.rechazar-solicitud');
